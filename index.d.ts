75f3d62c903d50eaaf7e57f88ec0f0633c322590
wei <49566929+theweiweiway@users.noreply.github.com>
2020-03-31 18:24:42 -0400

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare module "react-google-places-autocomplete" {
+  import * as React from "react";
+
+  export { geocodeByAddress, geocodeByPlaceId, getLatLng };
+
+  export default GooglePlacesAutocomplete;
+  
+}
