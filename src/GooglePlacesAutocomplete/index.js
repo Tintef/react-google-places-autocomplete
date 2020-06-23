0b453359655153de7910faa8b32b99855f100f55
James Patillo <james.patillo@gmail.com>
2020-06-23 12:27:22 -0400

    fix function spelling and invalid reference
    Function initalizeService was misspelled. Three references to the function used the incorrect spelling, but one reference was using the correct spelling.  Fixed issue by changing function and valid references to use correct spelling.

--- src/GooglePlacesAutocomplete/index.js
+++ src/GooglePlacesAutocomplete/index.js
@@ -47,7 +47,7 @@ class GooglePlacesAutocomplete extends React.Component {
       injectScript(apiKey);
     }
 
-    this.initalizeService();
+    this.initializeService();
     document.addEventListener('click', this.handleClick);
   }
 
@@ -64,16 +64,16 @@ class GooglePlacesAutocomplete extends React.Component {
     }
   }
 
-  initalizeService = () => {
+  initializeService = () => {
     if (!window.google) {
       console.error('[react-google-places-autocomplete]: Google script not loaded'); // eslint-disable-line no-console
-      setTimeout(this.initalizeService, 500);
+      setTimeout(this.initializeService, 500);
       return;
     }
 
     if (!window.google.maps) {
       console.error('[react-google-places-autocomplete]: Google maps script not loaded'); // eslint-disable-line no-console
-      setTimeout(this.initalizeService, 500);
+      setTimeout(this.initializeService, 500);
       return;
     }
 
