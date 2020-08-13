f20ef9f15bd5d6f306053e98cb4b38e59c709f92
Nicolás Tinte <nico.tinte@gmail.com>
2020-08-12 22:25:53 -0300

    Remove previous version files and dependencies

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import GooglePlacesAutocomplete from './GooglePlacesAutocomplete';
-import { geocodeByAddress, geocodeByPlaceId, getLatLng } from './utils/googleGeocodesHelper';
-
-export { geocodeByAddress, geocodeByPlaceId, getLatLng };
-export default GooglePlacesAutocomplete;
