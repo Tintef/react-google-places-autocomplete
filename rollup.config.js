b94ff185bb64de6bff12d6a1447264cfe5fa0959
Nicolás Tinte <nico.tinte@gmail.com>
2020-08-24 00:08:08 -0300

    [feature] Update build configs.

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,18 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 
-import packageJson from "./package.json";
+import pkg from "./package.json";
 
 export default {
   input: "src/index.ts",
   output: [
     {
-      file: packageJson.main,
+      file: pkg.main,
       format: "cjs",
       sourcemap: true
     },
     {
-      file: packageJson.module,
+      file: pkg.module,
       format: "esm",
       sourcemap: true
     }
@@ -23,6 +23,6 @@ export default {
     peerDepsExternal(),
     resolve(),
     commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript(),
   ],
 };
