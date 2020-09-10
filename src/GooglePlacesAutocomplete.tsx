3ae2a76252e793f93a7a387523c086e517850af9
bahaa kallas <bahaa.kallas@moshbit.com>
2020-09-10 16:59:02 +0200

    Replaced Async by AsyncCreatable

--- src/GooglePlacesAutocomplete.tsx
+++ src/GooglePlacesAutocomplete.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import AsyncSelect from 'react-select/async';
 import { OptionsType, OptionTypeBase } from 'react-select';
 import { useDebouncedCallback } from 'use-debounce';
 import GooglePlacesAutocompleteProps, { AutocompletionRequest }from './GooglePlacesAutocomplete.types';
 import injectScript from './helpers/injectScript';
+import AsyncCreatable from "react-select/async-creatable";
 import autocompletionRequestBuilder from './helpers/autocompletionRequestBuilder';
 
 const GooglePlacesAutocomplete: React.FC<GooglePlacesAutocompleteProps> = ({
@@ -58,8 +58,9 @@ const GooglePlacesAutocomplete: React.FC<GooglePlacesAutocompleteProps> = ({
   }, []);
 
   return (
-    <AsyncSelect
+    <AsyncCreatable
       {...selectProps}
+      formatCreateLabel={(value) => value}
       loadOptions={fetchSuggestions}
       getOptionValue={({ value }) => value.place_id}
     />
