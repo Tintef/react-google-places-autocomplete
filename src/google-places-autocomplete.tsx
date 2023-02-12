ce89330fc66ff0be02a6c64ee122e1a9bb9bcc8e
Nicolás Tinte <nicolas.tinte@airops.com>
2023-02-12 19:36:52 -0300

    refactor: hooks everywhere!

--- src/google-places-autocomplete.tsx
+++ src/google-places-autocomplete.tsx
@@ -1,52 +1,30 @@
-import React, { forwardRef, useEffect, useImperativeHandle, useState } from 'react';
+import React, { forwardRef, useImperativeHandle } from 'react';
 import AsyncSelect from 'react-select/async';
-import { Options } from 'react-select';
-import { useDebouncedCallback } from 'use-debounce';
-import { Loader } from '@googlemaps/js-api-loader';
 
 import GooglePlacesAutocompleteProps, {
-  AutocompletionRequest,
   GooglePlacesAutocompleteHandle,
 } from './types';
-import autocompletionRequestBuilder from './helpers/autocompletion-request-builder';
+import usePlacesService from './hooks/use-places-service';
+import useFetchSuggestions from './hooks/use-fetch-suggestions';
 
-const GooglePlacesAutocomplete: React.ForwardRefRenderFunction<GooglePlacesAutocompleteHandle, GooglePlacesAutocompleteProps> = ({
-  apiKey = '',
-  apiOptions = {},
-  autocompletionRequest = {},
-  debounce = 300,
-  minLengthAutocomplete = 0,
-  selectProps = {},
-  onLoadFailed = console.error,
-  withSessionToken = false,
-} : GooglePlacesAutocompleteProps, ref) : React.ReactElement => {
-  const [placesService, setPlacesService] = useState<google.maps.places.AutocompleteService | undefined>(undefined);
-  const [sessionToken, setSessionToken] = useState<google.maps.places.AutocompleteSessionToken | undefined>(undefined);
-  const [fetchSuggestions] = useDebouncedCallback((value: string, cb: (options: Options<any>) => void): void => {
-    if (!placesService) return cb([]);
-    if (value.length < minLengthAutocomplete) return cb([]);
+const GooglePlacesAutocomplete: React.ForwardRefRenderFunction<GooglePlacesAutocompleteHandle, GooglePlacesAutocompleteProps> = (
+  args: GooglePlacesAutocompleteProps,
+  ref,
+) : React.ReactElement => {
 
-    const autocompletionReq: AutocompletionRequest = { ...autocompletionRequest };
-
-    placesService.getPlacePredictions(
-      autocompletionRequestBuilder(
-        autocompletionReq,
-        value,
-        withSessionToken && sessionToken,
-      ), (suggestions) => {
-        cb((suggestions || []).map(suggestion => ({ label: suggestion.description, value: suggestion })));
-      },
-    );
-  }, debounce);
-
-  const initializeService = () => {
-    if (!window.google) throw new Error('[react-google-places-autocomplete]: Google script not loaded');
-    if (!window.google.maps) throw new Error('[react-google-places-autocomplete]: Google maps script not loaded');
-    if (!window.google.maps.places) throw new Error('[react-google-places-autocomplete]: Google maps places script not loaded');
-
-    setPlacesService(new window.google.maps.places.AutocompleteService());
-    setSessionToken(new google.maps.places.AutocompleteSessionToken());
-  }
+  const { placesService, sessionToken, setSessionToken } = usePlacesService({
+    apiKey: args.apiKey ?? '',
+    apiOptions: args.apiOptions ?? {},
+    onLoadFailed: args.onLoadFailed ?? console.error,
+  });
+  const fetchSuggestions = useFetchSuggestions({
+    autocompletionRequest: args.autocompletionRequest ?? {},
+    debounce: args.debounce ?? 300,
+    minLengthAutocomplete: args.minLengthAutocomplete ?? 0,
+    placesService,
+    sessionToken,
+    withSessionToken: args.withSessionToken ?? false,
+  });
 
   useImperativeHandle(ref, () => ({
     getSessionToken: () => {
@@ -57,25 +35,9 @@ const GooglePlacesAutocomplete: React.ForwardRefRenderFunction<GooglePlacesAutoc
     }
   }), [sessionToken]);
 
-  useEffect(() => {
-    const init = async () => {
-      try {
-        if ( !window.google || !window.google.maps || !window.google.maps.places ) {
-          await new Loader({ apiKey, ...{ libraries: ['places'], ...apiOptions }}).load();
-        }
-        initializeService();
-      } catch (error) {
-        onLoadFailed(error);
-      }
-    }
-
-    if (apiKey) init();
-    else initializeService();
-  }, []);
-
   return (
     <AsyncSelect
-      {...selectProps}
+      {...args.selectProps ?? {}}
       loadOptions={fetchSuggestions}
       getOptionValue={({ value }) => value.place_id}
     />
